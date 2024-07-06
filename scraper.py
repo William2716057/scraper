9ad14d559aa41750bba59e68b885d2105f1a950c
William <willspri@gmail.com>
2024-07-06 16:25:12 +1200

    save to CSV

--- scraper.py
+++ scraper.py
@@ -1,10 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 page_to_scrape = requests.get("https://quotes.toscrape.com/")
 soup = BeautifulSoup(page_to_scrape.text, "html.parser")
 quotes = soup.find_all("span", attrs={"class":"text"})
 authors = soup.find_all("small", attrs={"class":"author"})
 
+file = open("scraped_quotes.csv", "w")
+
+writer = csv.writer(file)
+writer.writerow(['QUOTES", "AUTHORS'])
+
 for quote, author in zip(quotes, authors):
     print(quote.text + " - " + author.text)
+file.close()
